4d6d812a1d678339c3a13d21b918a17509855e39
AbdelrahmanQotb3 <abdelrahmanmahmoud1772@gmail.com>
2024-11-25 22:38:33 +0200

    Modifying.

--- lib/UI/Screens/Splash/splash.dart
+++ lib/UI/Screens/Splash/splash.dart
@@ -17,7 +17,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(Duration(seconds: 5), (){
+    Future.delayed(Duration(seconds: 2), (){
       Navigator.pushReplacementNamed(context, Login.routeName);
     });
   }
