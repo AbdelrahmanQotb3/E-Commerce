e9389c4bd0325717f009ca80f9df711cd7bd2358
AbdelrahmanQotb3 <abdelrahmanmahmoud1772@gmail.com>
2024-12-15 16:03:02 +0200

    Created API's strings class.

---
+++ lib/Utilities/end_points.dart
@@ -0,0 +1,5 @@
+abstract class EndPoints{
+  static String baseURL = "https://ecommerce.routemisr.com/";
+  static String login = "${baseURL}api/v1/auth/signin";
+  static String signUp = "${baseURL}api/v1/auth/signup";
+}
